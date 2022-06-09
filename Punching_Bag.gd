f6293b3d8d5d80d0e88e6a1bc3ef3f5396400b8b
Lu <Mluisa5365@gmail.com>
2022-06-09 13:24:00 -0400

    Finish with damage indicator

--- Punching_Bag.gd
+++ Punching_Bag.gd
@@ -1,9 +1,17 @@
 extends StaticBody2D
 
-
+var floatingtext = preload("res://FloatingText.tscn")
 
 func _on_Hurtbox_area_entered(area):
 	if area.is_in_group("TYPE_HITBOX") and area.is_in_group("PUNCH"):
 		print("damaged from punch")
+		punch_hit()
 	if area.is_in_group("TYPE_HITBOX") and area.is_in_group("KICK"):
 		print("damaged from kick")
+
+func punch_hit():
+	var damage = 10
+	var text = floatingtext.instance()
+	text.amount = damage
+	print(text.amount)
+	get_node("Position2D").add_child(text)
