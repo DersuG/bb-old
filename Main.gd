5cbf0976b5fb7272a41d60a0597387de81b743c7
Lu <Mluisa5365@gmail.com>
2022-06-10 12:48:04 -0400

    added multitarget camera

--- Main.gd
+++ Main.gd
@@ -5,3 +5,7 @@ extends Node
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit(0)
+
+func _ready():
+	$Gameplay/MultiTargetCam.add_target($Gameplay/YSort/Character)
+	$Gameplay/MultiTargetCam.add_target($Gameplay/YSort/Player2)
